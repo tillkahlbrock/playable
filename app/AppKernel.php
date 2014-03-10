40e73601d173d951744e76fc9b61847c0821f2bd
Till Kahlbrock <till.kahlbrock@gmail.com>
2014-03-10 09:04:27 +0100

    adds and sets up fos user bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new PB\PlayableBundle\PBPlayableBundle(),
+            new \FOS\UserBundle\FOSUserBundle(),
+            new PB\UserBundle\PBUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
